d31f75eb68b4e6ae1e0954d31f0cbb38c378eda8
innapau <jemmek364@gmail.com>
2018-07-31 15:10:45 +0300

    BrainGames project step two

---
+++ src/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('\nMay I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+};
+export default greetings;
